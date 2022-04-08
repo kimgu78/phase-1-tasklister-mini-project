d9d197e7719c49720f0ea60c900439fbc42b6ff3
kimgu78 <kimgu78@gmail.com>
2022-04-08 14:27:47 -0600

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskArray = [];
+  const addTask = (task) => taskArray.push(task);
+  const removeTask = (task) => taskArray.splice;
+
+  document
+    .getElementById("create-task-form")
+    .addEventListener("submit", (e) => {
+      const li = document.createElement("li");
+      li.innerText = document.getElementById("new-task-description").value;
+      document.getElementById("tasks").appendChild(li);
+      const btn = document.createElement("button");
+      btn.innerHTML = "X";
+      li.append(btn);
+      btn.addEventListener("click", (e) => {
+        li.remove();
+      });
+      e.preventDefault();
+    });
 });
